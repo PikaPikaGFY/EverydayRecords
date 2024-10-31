1ebd739cdbd9f34f0982bdf84dc14d56ffab6ea0
pikapikagfy <853026567@qq.com>
2024-10-31 10:41:32 +0800

    cargo check

---
+++ Rustlearning/hello_world/target/debug/deps/hello_world-5990f2d3fc4ff433.d
@@ -0,0 +1,5 @@
+D:\EverydayRecords\Rustlearning\hello_world\target\debug\deps\libhello_world-5990f2d3fc4ff433.rmeta: src/main.rs
+
+D:\EverydayRecords\Rustlearning\hello_world\target\debug\deps\hello_world-5990f2d3fc4ff433.d: src/main.rs
+
+src/main.rs:
--- Rustlearning/hello_world/target/debug/hello_world.d
+++ Rustlearning/hello_world/target/debug/hello_world.d
@@ -1,1 +1,1 @@
-E:\EverydayRecords\Rustlearning\hello_world\target\debug\hello_world.exe: E:\EverydayRecords\Rustlearning\hello_world\src\main.rs
+D:\EverydayRecords\Rustlearning\hello_world\target\debug\hello_world.exe: D:\EverydayRecords\Rustlearning\hello_world\src\main.rs
