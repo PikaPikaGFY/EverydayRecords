3d92e0590eb04c560731bcadfc37871c98c465a3
PikaPikaGFY <853026567@qq.com>
2024-10-31 10:02:19 +0800

    rust learning

---
+++ Rustlearning/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+E:\EverydayRecords\Rustlearning\hello_world\target\debug\deps\hello_world.exe: src/main.rs
+
+E:\EverydayRecords\Rustlearning\hello_world\target\debug\deps\hello_world.d: src/main.rs
+
+src/main.rs:
---
+++ Rustlearning/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+E:\EverydayRecords\Rustlearning\hello_world\target\debug\hello_world.exe: E:\EverydayRecords\Rustlearning\hello_world\src\main.rs
